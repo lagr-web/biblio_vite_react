ade68c64ffb762cc0dd5220b391f39d5e5eaaf63
lagr-web <manglerbarelige@gmail.com>
2026-09-19 21:33:42 +0200

    cleanup

--- src/handlers/adminHandlers.js
+++ src/handlers/adminHandlers.js
@@ -1,5 +1,10 @@
 // src/routes/adminRoute.js
-import { getAllBookData, getAllGenreData, deleteBookData, createBookData } from "../data.js";
+import {
+  getAllBookData,
+  getAllGenreData,
+  deleteBookData,
+  createBookData,
+} from "../data.js";
 
 export const adminLoader = async () => {
   const [books, genres] = await Promise.all([
@@ -9,18 +14,16 @@ export const adminLoader = async () => {
   return { books, genres };
 };
 
-
 export const adminAction = async ({ request }) => {
-
   const formData = await request.formData();
 
   //Håndter Slet (DELETE)
   if (request.method === "DELETE") {
     const bookId = formData.get("id");
-    return await deleteBookData(bookId); 
+    return await deleteBookData(bookId);
   }
 
   if (request.method === "POST") {
-    return await createBookData(formData); 
+    return await createBookData(formData);
   }
 };
